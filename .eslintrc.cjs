80062c4113ab143d869f89e6754488e87dd048d0
llliuyang <1714610279@qq.com>
2022-11-18 11:08:21 +0800

    配置Eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        // 环境，针对哪些环境的语法
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        // 集成了哪些规则，别人写好的规则直接拿来用
+        "eslint:recommended",
+        "plugin:vue/vue3-essential",
+        "plugin:@typescript-eslint/recommended" // typescript 规则
+    ],
+    "overrides": [
+    ],
+    // "parser": "@typescript-eslint/parser",
+    "parser": "vue-eslint-parser", // 解析.vue文件
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser", // 解析.ts文件
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        // 自定义的规则写在这里
+    }
+}
