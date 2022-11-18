afbfa47f7b0815b5fa874f2aa2f1d72609688717
llliuyang <1714610279@qq.com>
2022-11-18 11:20:59 +0800

    配置Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * 此文件是为了让 Prettier 插件能够识别用户配置，配置需与.eslintrc.js 中保持一致
+ */
+module.exports = {
+	singleQuote: false, // 使⽤单引号
+	semi: false, // 末尾添加分号
+	tabWidth: 2,
+	trailingComma: "none",
+	useTabs: false,
+	endOfLine: "auto",
+};
